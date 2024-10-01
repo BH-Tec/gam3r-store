c11ab5d50f5f06b8c26f5ba8a5f337e1dc35e67a
Bruno Henrique <brunodorea@outlook.com.br>
2024-10-01 18:16:20 -0300

    :lipstick: feat: ajustando cor do background da pagina

--- essencial/frontend/src/app/page.tsx
+++ essencial/frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 import ProdutoItem from '@/components/produto/ProdutoItem'
 import { produtos } from '@/core'
 
-export default function Inicio() {
+export default function Home() {
     return (
-        <div className="grid grid-cols-4 gap-5 container">
-            {produtos.map((produto) => (
-                <ProdutoItem key={produto.id} produto={produto} />
-            ))}
+        <div className='bg-violet-dark'>
+            <div className="grid grid-cols-4 gap-5 container">
+                {produtos.map((produto) => (
+                    <ProdutoItem key={produto.id} produto={produto} />
+                ))}
+            </div>
         </div>
     )
 }
